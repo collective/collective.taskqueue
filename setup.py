0be8e1a771d31186fd3243b8239a414bc986bb60
Asko Soukka <asko.soukka@iki.fi>
2013-11-25 07:45:27 +0200

    Preparing release 0.4.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.taskqueue',
-    version='0.4.4.dev0',
+    version='0.4.4',
     description='',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.txt').read()),
