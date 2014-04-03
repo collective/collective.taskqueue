4ab1f0c12e45161634300e6aef1084cacb1a9078
Asko Soukka <asko.soukka@iki.fi>
2014-04-03 09:32:21 +0300

    Back to development: 0.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.taskqueue',
-    version='0.5.0',
+    version='0.5.1.dev0',
     description='',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.txt').read()),
