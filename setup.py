203e40a9c1c1e7a83beb1372fb458788e228867a
Asko Soukka <asko.soukka@iki.fi>
2017-01-03 06:20:44 +0200

    Back to development: 0.8.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.taskqueue',
-    version='0.8.2',
+    version='0.8.3.dev0',
     description='Asyncore-based asynchronous task queue for Plone',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.txt').read()),
